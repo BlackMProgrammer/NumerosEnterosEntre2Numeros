a0722a97ac2fabe372092dd4b1954f95473ffae6
BlackMProgrammer <cristhiancastro01@gmail.com>
2020-09-08 12:40:45 -0400

    Add project files.

---
+++ NumerosEnterosEntre2Numeros/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NumerosEnterosEntre2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite el Primer numero");
+            string numero1val = Console.ReadLine();
+            int numero1 = Convert.ToInt32(numero1val);
+
+            Console.WriteLine("Digite el segundo numero");
+            string numero2val = Console.ReadLine();
+            int numero2 = Convert.ToInt32(numero2val);
+
+            do
+            {
+                Console.WriteLine("{0}", numero1++);
+            } while (numero1 < numero2);
+        }
+    }
+}
